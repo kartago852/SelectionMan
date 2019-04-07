a52829f5c7d3f39e5f1ce9d152fe00ff12162dbe
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2019-04-07 02:36:04 -0500

    Commit Promedio

--- app/src/main/java/com/example/selectionman/MainActivity.java
+++ app/src/main/java/com/example/selectionman/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.selectionman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int mate = 5;
+        int quimi = 5;
+        int fisica = 5;
+        int promedio = 0;
+
+        promedio = (mate + quimi + fisica) /3;
+
+        if(promedio <= 6){
+            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
+        } else if(promedio <= 5){
+            Toast.makeText(this, "Reprobado", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
