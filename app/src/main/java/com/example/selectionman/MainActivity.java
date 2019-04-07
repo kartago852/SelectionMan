f393c968842896d658271196a7d3a75716c48887
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2019-04-07 17:11:28 -0500

    Commit Test Sum 1

--- app/src/main/java/com/example/selectionman/MainActivity.java
+++ app/src/main/java/com/example/selectionman/MainActivity.java
@@ -2,27 +2,42 @@ package com.example.selectionman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int mate = 5;
-        int quimi = 5;
-        int fisica = 5;
-        int promedio = 0;
+        et1 = (EditText)findViewById(R.id.txt_num1);
+        et2 = (EditText)findViewById(R.id.txt_num2);
 
-        promedio = (mate + quimi + fisica) /3;
-
-        if(promedio <= 6){
-            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
-        } else if(promedio <= 5){
-            Toast.makeText(this, "Reprobado", Toast.LENGTH_SHORT).show();
-        }
+        tv1 = (TextView)findViewById(R.id.txt_result);
     }
+
+        public void Sumar(View view){
+            String valor1 = et1.getText().toString();
+            String valor2 = et2.getText().toString();
+
+            int num1 = Integer.parseInt(valor1);
+            int num2 = Integer.parseInt(valor2);
+
+            int suma = num1 + num2;
+
+            String result = String.valueOf(suma);
+
+            tv1.setText(result);
+
+        }
 
 }
